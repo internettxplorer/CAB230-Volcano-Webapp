8334330ece067d88dcefc26fc6d4a4725bafa72b
internettxplorer <internette.explorer@protonmail.ch>
2024-04-26 00:16:27 +1000

    remove unnecessary comments from BarGraph.jsx

--- src/components/BarGraph.jsx
+++ src/components/BarGraph.jsx
@@ -2,18 +2,12 @@ import { Box, Text } from "@mantine/core";
 import { BarChart } from "@mantine/charts";
 import PropTypes from 'prop-types';
 import { useLoaderData } from "react-router-dom";
-import { checkLoggedIn } from "../helpers/UserAuth";
 
 /**
- * @desc Conditionally renders bar graph with population density info if user is logged in
- * 
- * @todo ...
+ * @desc Display bar graph with population data (given if user logged in)
  */
-export default function BarGraph() { // put in separate file BarGraph.jsx
+export default function BarGraph() {
     const volcano = useLoaderData();
-
-    // BarGraph({ isLoggedIn })
-    // console.log(checkLoggedIn()); // DELETE
     const populationData = [
         {range: "5 km", density: volcano.population_5km},
         {range: "10 km", density: volcano.population_10km},
@@ -28,7 +22,7 @@ export default function BarGraph() { // put in separate file BarGraph.jsx
             </Text>
             <BarChart 
                 h={300}
-                data={populationData} // replace later
+                data={populationData}
                 dataKey="range"
                 xAxisLabel="Distance from volcano"
                 valueFormatter={(value) => new Intl.NumberFormat('en-AU').format(value)}
